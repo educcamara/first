994b71ea23ecd10d1de4eb33866f8c5cda9d921d
Eduardo <139073033+educcamara@users.noreply.github.com>
2023-10-04 21:28:27 -0400

    Update matriz.py

--- matriz.py
+++ matriz.py
@@ -2,23 +2,23 @@
     Arquivo Python com funções para manipulação de matrizes.
 """
 
-print('Digite a primeira matriz em forma de lista de listas:')
-m1 = eval(input(' > '))
-print('Digite a segunda matriz em forma de lista de listas:')
-m2 = eval(input(' > ')) 
+print("Digite a primeira matriz em forma de lista de listas:")
+m1 = eval(input(" > "))
+print("Digite a segunda matriz em forma de lista de listas:")
+m2 = eval(input(" > "))
 
 matriz3 = []
 
-for i in range(len(m1)):
+for i, row in enumerate(m1):
     matriz3.append([])
-    for k in range(len(m2[0])):
-        elem = 0
-        for j in range(len(m2)):
-            elem += (m1[i][j] * m2[j][k])
+    for k, col in enumerate(m2[0]):
+        result = 0
+        for j, val in enumerate(m2):
+            result += row[j] * val[k]
             # print(f'a{i+1}{j+1} b{j+1}{k+1}')
-        matriz3[i].append(elem)
+        matriz3[i].append(result)
 
-print('A multiplicação das duas matrizes resultou em:')
+print("A multiplicação das duas matrizes resultou em:")
 print(matriz3)
 
-# exemplo: [[1,2],[2,1]]   [[1,0],[0,1]]
+# exemplo: [[1,2],[2,1]] [[1,0],[0,1]]
