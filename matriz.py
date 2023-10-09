122785ed6528a4484c2e6d339e12b44ae582bb6d
Eduardo <139073033+educcamara@users.noreply.github.com>
2023-10-08 22:30:36 -0400

    Classe Matrix

--- matriz.py
+++ matriz.py
@@ -8,7 +8,7 @@ def identity_matrix(n: int) -> [[int]]:
     Funcao que cria uma matriz identidade de ordem n.
     Retorna uma matriz.
     """
-    matrix =[]
+    matrix = []
     for i in range(n):
         matrix.append([])
         for j in range(n):
@@ -16,52 +16,6 @@ def identity_matrix(n: int) -> [[int]]:
                 matrix[i].append(0)
             else:
                 matrix[i].append(1)
-
-    return matrix
-
-
-def determinant(matrix: [[float]]) -> float:
-    """
-    Funcao que calcula o determinante de uma matriz identidade de ordem n.
-    Retorna um numero.
-    """
-    if len(matrix) != len(matrix[0]):
-        return None
-    if len(matrix) == 1:
-        return matrix[0][0]
-    
-    det = 0
-    for j, _ in enumerate(matrix[0]):
-        new_matrix = [matrix[i+1][:j] + matrix[i+1][j+1:] for i, _ in enumerate(matrix[1:])]
-        # for i, _ in enumerate(matrix[1:]):
-        #     new_matrix.append(matrix[i+1][:j] + matrix[i+1][j+1:])
-        # print(new_matrix)
-        if j % 2 == 0:
-            det += matrix[0][j] * determinant(new_matrix)
-        else:
-            det -= matrix[0][j] * determinant(new_matrix)
-
-    return det
-    
-
-def create_matrix() -> [[float]]:
-    """
-    Funcao que cria uma matriz.
-    Retorna uma matriz.
-    """
-    matrix = []
-    while True:
-        row = input(" > ")
-        if not row:
-            break
-        row = [float(x) for x in row.split()]
-        if matrix:
-            if len(row) < len(matrix[0]):
-                row += [0] * (len(matrix[0]) - len(row))
-            elif len(row) > len(matrix[0]):
-                for i, _ in enumerate(matrix):
-                    matrix[i] += [0] * (len(row) - len(matrix[i]))
-        matrix.append(row)
 
     return matrix
 
@@ -142,12 +96,130 @@ def multiply_matrix(matrix1: [[float]], matrix2: [[float]]) -> [[float]]:
     return matrix
 
 
-def main():
+class Matrix:
     """
-    Funcao principal.
+    Classe Matrix
     """
 
-    return 0
+    from typing import Tuple
+
+    def __init__(self, matrix: [[float]]):
+        self.matrix = matrix
+        self.determinant = self._calc_determinant(self.matrix)
+        self.size = (len(matrix), len(matrix[0]))
+
+    def __getitem__(self, coords: Tuple[int, int]):
+        i = coords[0]
+        j = coords[1]
+        try:
+            return self.matrix[i][j]
+        except IndexError:
+            return None
+
+    def __setitem__(self, coords: Tuple[int, int], value):
+        i = coords[0]
+        j = coords[1]
+        try:
+            self.matrix[i][j] = float(value)
+        except IndexError:
+            pass
+
+    def __repr__(self) -> str:
+        text = ""
+        for row in self.matrix:
+            text += f"{row}\n"
+        return text
+
+    def __add__(self, item):
+        matrix = []
+        if isinstance(item, (int, float)):
+            for i, row in enumerate(self.matrix):
+                matrix.append([])
+                for elem in row:
+                    matrix[i].append(elem + item)
+
+        elif isinstance(item, Matrix):
+            if self.size != item.size:
+                raise ValueError("Matrices with different sizes.")
+
+            for i, _ in enumerate(self.matrix):
+                matrix.append([])
+                for j, _ in enumerate(self.matrix[0]):
+                    matrix[i].append(self.matrix[i][j] + item[i, j])
+
+        return Matrix(matrix)
+
+    def __mul__(self, item):
+        matrix = []
+        if isinstance(item, (int, float)):
+            for i, row in enumerate(self.matrix):
+                matrix.append([])
+                for elem in row:
+                    matrix[i].append(elem * item)
+
+    def __sub__(self, item):
+        matrix = []
+        if isinstance(item, (int, float)):
+            for i, row in enumerate(self.matrix):
+                matrix.append([])
+                for elem in row:
+                    matrix[i].append(elem - item)
+
+        elif isinstance(item, Matrix):
+            if self.size != item.size:
+                raise ValueError("Matrices with different sizes.")
+
+            for i, _ in enumerate(self.matrix):
+                matrix.append([])
+                for j, _ in enumerate(self.matrix[0]):
+                    matrix[i].append(self.matrix[i][j] - item[i, j])
+
+        return Matrix(matrix)
+
+    def __iter__(self):
+        return iter(self.matrix)
+
+    def _calc_determinant(self, matrix) -> float:
+        """
+        Funcao que calcula o determinante de uma matriz identidade de ordem n.
+        Retorna um numero.
+        """
+        if len(matrix) != len(matrix[0]):
+            return None
+        if len(matrix) == 1:
+            return matrix[0][0]
+
+        det = 0
+        for j, _ in enumerate(matrix[0]):
+            new_matrix = [
+                matrix[i + 1][:j] + matrix[i + 1][j + 1 :]
+                for i, _ in enumerate(matrix[1:])
+            ]
+            if j % 2 == 0:
+                det += matrix[0][j] * self._calc_determinant(new_matrix)
+            else:
+                det -= matrix[0][j] * self._calc_determinant(new_matrix)
+
+        return det
 
 
-main()
+def create_matrix() -> Matrix:
+    """
+    Funcao que cria uma matriz.
+    Retorna uma matriz.
+    """
+    matrix = []
+    while True:
+        row = input(" > ")
+        if not row:
+            break
+        row = [float(x) for x in row.split()]
+        if matrix:
+            if len(row) < len(matrix[0]):
+                row += [0] * (len(matrix[0]) - len(row))
+            elif len(row) > len(matrix[0]):
+                for i, _ in enumerate(matrix):
+                    matrix[i] += [0] * (len(row) - len(matrix[i]))
+        matrix.append(row)
+
+    return Matrix(matrix)
