e26fe7610d61b90dc5e405a98721a69552382781
Eduardo <139073033+educcamara@users.noreply.github.com>
2023-10-15 12:51:49 -0400

    feat: turtlegraph.py

---
+++ turtlegraph.py
@@ -0,0 +1,14 @@
+"""
+Arquivo que utiliza a biblioteca `turtle` para criar simples funçōes em gráficos
+"""
+import turtle
+
+def initialize_graph():
+    """
+    Inicializa o gráfico na janela turtle
+    """
+    screen = turtle.Screen()
+    screen.screensize(500, 500)
+    pen = turtle.Turtle()
+
+
